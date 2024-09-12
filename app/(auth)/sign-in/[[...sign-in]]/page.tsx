8d45af27c0c37436399bb0e0b93e0548c3b566c9
Falasefemi2 <femifalase228@gmail.com>
2024-09-12 06:55:10 +0100

    change base color

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,9 +1,17 @@
 import { SignIn } from "@clerk/nextjs";
+import { dark } from '@clerk/themes'
+
 
 export default function Page() {
     return (
         <div className="w-full min-h-screen flex justify-center items-center">
-            <SignIn />
+            <SignIn
+                appearance={{
+                    baseTheme: dark,
+                }}
+                path="/sign-in"
+            />
+
         </div>
     );
 }
--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -1,9 +1,15 @@
 import { SignUp } from "@clerk/nextjs";
+import { dark } from '@clerk/themes'
 
 export default function Page() {
     return (
         <div className="w-full min-h-screen flex justify-center items-center">
-            <SignUp />
+            <SignUp
+                appearance={{
+                    baseTheme: dark,
+                }}
+                path="/sign-up"
+            />
         </div>
     );
 }

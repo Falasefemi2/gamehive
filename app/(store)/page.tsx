eff9e702be0a50dbc643eb960d9f06ef0184b8c0
Falasefemi2 <femifalase228@gmail.com>
2024-09-15 15:05:08 +0100

    redirect to sign-in

--- app/(store)/page.tsx
+++ app/(store)/page.tsx
@@ -1,9 +1,17 @@
 import { BannerGames } from "@/components/FeaturedGames/BannerGames";
 import { getSevenGameThumbnails } from "@/app/action";
+import { auth } from "@clerk/nextjs/dist/types/server";
+import { redirect } from "next/navigation";
 
 
 export default async function Home() {
     const games = await getSevenGameThumbnails();
+
+    const { userId } = auth();
+
+    if (!userId) {
+        redirect("/sign-in");
+    }
 
     return (
         <>

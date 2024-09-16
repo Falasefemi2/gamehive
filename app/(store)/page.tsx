911c058e2f60041419f933fe141a6aaa731f6f3e
Falasefemi2 <femifalase228@gmail.com>
2024-09-16 12:52:13 +0100

    updated middleware

--- app/(store)/page.tsx
+++ app/(store)/page.tsx
@@ -1,18 +1,8 @@
 import { BannerGames } from "@/components/FeaturedGames/BannerGames";
 import { getSevenGameThumbnails } from "@/app/action";
 
-import { redirect } from "next/navigation";
-import { auth } from "@clerk/nextjs/server";
-
-
 export default async function Home() {
     const games = await getSevenGameThumbnails();
-
-    const { userId } = auth();
-
-    if (!userId) {
-        redirect("/sign-in");
-    }
 
     return (
         <>

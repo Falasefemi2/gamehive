458295fae5b5ccf3de531db49d42a0ebf221a2c1
Falasefemi2 <femifalase228@gmail.com>
2024-09-18 17:15:49 +0100

    check user

--- components/Navigation/Navbar/index.tsx
+++ components/Navigation/Navbar/index.tsx
@@ -2,11 +2,14 @@ import { UserButton } from '@clerk/nextjs'
 import React from 'react'
 import MobileNav from '../Sidebar/MobileNav'
 import SearchBar from '../Search'
+import { CheckUser } from '@/lib/checkUser'
 
 // import { SearchBar } from '..'
 
 
-function Navbar() {
+async function Navbar() {
+    const user = await CheckUser()
+
     return (
         <div className="flex justify-between items-center mb-8 gap-4">
             <div className="flex gap-4">
@@ -16,11 +19,13 @@ function Navbar() {
 
                 <SearchBar basePath="search" />
             </div>
-            <UserButton
-                afterSignOutUrl="/"
-                userProfileMode="navigation"
-                userProfileUrl="/user-profile"
-            />
+            {user && (
+                <UserButton
+                    afterSignOutUrl="/"
+                    userProfileMode="navigation"
+                    userProfileUrl="/user-profile"
+                />
+            )}
         </div>
     )
 }
---
+++ lib/checkUser.ts
@@ -0,0 +1,41 @@
+/** @format */
+
+import { db, users } from "@/server/schema";
+import { currentUser } from "@clerk/nextjs/server";
+import { eq } from "drizzle-orm";
+
+export async function CheckUser() {
+  try {
+    const user = await currentUser();
+    if (!user) {
+      return null;
+    }
+    // Check if user is in db
+    const existingUser = await db
+      .select()
+      .from(users)
+      .where(eq(users.id, user.id))
+      .limit(1);
+
+    if (existingUser.length > 0) {
+      // User already exists in the database
+      return existingUser[0];
+    } else {
+      // User doesn't exist, create a new one
+      const newUser = {
+        authId: user.id,
+        email: user.emailAddresses[0]?.emailAddress ?? "",
+        profilePictureUrl: user.imageUrl ?? "",
+        username: user.username ?? `user_${user.id}`, // Generate a username if not provided
+        firstName: user.firstName ?? "",
+        lastName: user.lastName ?? "",
+      };
+
+      const insertedUser = await db.insert(users).values(newUser).returning();
+      return insertedUser[0];
+    }
+  } catch (error) {
+    console.error("Error in CheckUser:", error);
+    return null;
+  }
+}

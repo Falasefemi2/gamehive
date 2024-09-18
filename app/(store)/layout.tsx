025f68b994a5ac13fa958ec6d84cc5d9547a50fc
Falasefemi2 <femifalase228@gmail.com>
2024-09-18 23:10:52 +0100

    remove some text

--- app/(store)/layout.tsx
+++ app/(store)/layout.tsx
@@ -11,9 +11,6 @@ function Layout({ children }: { children: React.ReactNode }) {
                 <div className="p-4 py-4 flex-grow max-w-[1440px] mx-auto md:p-8 md:py-4  lg:p-16 lg:py-8">
                     <Navbar />
                     {children}
-                    <div className=" text-sm text-neutral-500 p-4 text-center">
-                        Built for learning purpuses, code availabe on GitHub
-                    </div>
                 </div>
             </div>
         </>
